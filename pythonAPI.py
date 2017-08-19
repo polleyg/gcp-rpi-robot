59f6f970f26f83d379f4976f69ec357e0187b8c1
AtomIsHere <AtomIsHere@users.noreply.github.com>
2017-08-19 02:06:40 +0000

    added le API for java server coming soonTM
    trying to run android app that will control the robot without wires

---
+++ pythonAPI.py
@@ -0,0 +1,25 @@
+from gopigo import *
+
+def foward():
+    fwd()
+    return
+
+def backwards():
+    bwd()
+    return
+
+def turnRight():
+    right()
+    return
+
+
+def stopM():
+    stop()
+    return
+
+def uTurn():
+    right()
+    right()
+    return
+
+
